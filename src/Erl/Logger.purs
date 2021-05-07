f65b241450796df8b1be528a5cb7b7ba8e0ddc08
srstrong <steve@srstrong.com>
2021-05-07 19:27:21 +0100

    Exported logLevelToErl

--- src/Erl/Logger.purs
+++ src/Erl/Logger.purs
@@ -17,6 +17,7 @@ module Logger
        , commandMetadata
        , eventMetadata
        , genericMetadata
+       , logLevelToErl
        , LogType(..)
        , LogLevel(..)
        , MinimalMetadata
